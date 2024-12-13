53de958ff17384643eaf6a717ed6edddc565fa6a
Crisiroid <csiroid@gmail.com>
2024-12-13 15:49:45 +0330

    Added BFS

---
+++ The8Puzzle/BFSSolver.cs
@@ -0,0 +1,120 @@
+﻿using System.Diagnostics;
+
+namespace The8Puzzle
+{
+    public class BFSSolver
+    {
+        private readonly int[,] startState;
+        private readonly int[,] goalState;
+        private readonly int rows = 3;
+        private readonly int cols = 3;
+
+        public BFSSolver(int[,] startState, int[,] goalState)
+        {
+            this.startState = startState;
+            this.goalState = goalState;
+        }
+
+        public List<int[,]> Solve()
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start(); 
+
+            var openList = new Queue<Node>();  
+            var closedList = new HashSet<string>(); 
+            openList.Enqueue(new Node(startState, null));
+
+            while (openList.Count > 0)
+            {
+                if (stopwatch.ElapsedMilliseconds > 30000) 
+                {
+                    Console.WriteLine("Algorithm isn't working with the input. Time exceeded 30 seconds.");
+                    return null;
+                }
+
+                var currentNode = openList.Dequeue(); 
+
+                if (IsGoal(currentNode.State))
+                    return ReconstructPath(currentNode);
+
+                closedList.Add(GetStateKey(currentNode.State));
+
+                int zeroRow = 0, zeroCol = 0;
+                for (int i = 0; i < rows; i++)
+                {
+                    for (int j = 0; j < cols; j++)
+                    {
+                        if (currentNode.State[i, j] == 0)
+                        {
+                            zeroRow = i;
+                            zeroCol = j;
+                        }
+                    }
+                }
+
+                var directions = new[] { (0, -1), (0, 1), (-1, 0), (1, 0) };
+                foreach (var (dx, dy) in directions)
+                {
+                    int newRow = zeroRow + dx;
+                    int newCol = zeroCol + dy;
+
+                    if (newRow >= 0 && newRow < rows && newCol >= 0 && newCol < cols)
+                    {
+                        var newState = (int[,])currentNode.State.Clone();
+                        newState[zeroRow, zeroCol] = newState[newRow, newCol];
+                        newState[newRow, newCol] = 0;
+
+                        if (closedList.Contains(GetStateKey(newState)))
+                            continue;
+
+                        openList.Enqueue(new Node(newState, currentNode)); 
+                    }
+                }
+            }
+
+            return null; 
+        }
+
+        private bool IsGoal(int[,] state)
+        {
+            for (int i = 0; i < rows; i++)
+            {
+                for (int j = 0; j < cols; j++)
+                {
+                    if (state[i, j] != goalState[i, j])
+                        return false;
+                }
+            }
+            return true;
+        }
+
+        private List<int[,]> ReconstructPath(Node node)
+        {
+            var path = new List<int[,]>();
+            while (node != null)
+            {
+                path.Add(node.State);
+                node = node.Parent;
+            }
+            path.Reverse(); 
+            return path;
+        }
+
+        private string GetStateKey(int[,] state)
+        {
+            return string.Join(",", state.Cast<int>());
+        }
+
+        private class Node
+        {
+            public int[,] State { get; }
+            public Node Parent { get; }
+
+            public Node(int[,] state, Node parent)
+            {
+                State = state;
+                Parent = parent;
+            }
+        }
+    }
+}
--- The8Puzzle/Program.cs
+++ The8Puzzle/Program.cs
@@ -4,6 +4,7 @@ class Program
 {
     static void Main(string[] args)
     {
+        //This input and IDA* don't work together. it takes more than 2 hours for IDA* to solve this input. 
         int[,] startState =
         {
             { 1, 2, 3 },
@@ -17,15 +18,15 @@ class Program
             { 4, 5, 6 },
             { 7, 8, 0 }
         };
+        var AStarsolver = new AStarSolver(startState, goalState);
+
+        var AStarSolution = AStarsolver.Solve();
+
         Console.WriteLine("__________________________A*__________________________");
-
-        var solver = new AStarSolver(startState, goalState);
-        var solution = solver.Solve();
-
-        if (solution != null)
+        if (AStarSolution != null)
         {
             Console.WriteLine("Solution found!");
-            foreach (var state in solution)
+            foreach (var state in AStarSolution)
             {
                 PrintState(state);
                 Console.WriteLine();
@@ -38,12 +39,29 @@ class Program
 
         Console.WriteLine("_________________________IDA*_________________________");
         var IDAsolver = new IDAStarSolver(startState, goalState);
-        var IDAsolution = solver.Solve();
+        var IDAsolution = IDAsolver.Solve();
 
         if (IDAsolution != null)
         {
-            Console.WriteLine("Solution found!");
+            Console.WriteLine("IDA* Solution found!");
             foreach (var state in IDAsolution)
+            {
+                PrintState(state);
+                Console.WriteLine();
+            }
+        }
+        else
+        {
+            Console.WriteLine("No solution found.");
+        }
+
+        Console.WriteLine("_________________________BFS______________________");
+        var BFSSolver = new BFSSolver(startState, goalState);
+        var BFSSolution = BFSSolver.Solve();
+        if (BFSSolution != null)
+        {
+            Console.WriteLine("BFS Solution found!");
+            foreach (var state in BFSSolution)
             {
                 PrintState(state);
                 Console.WriteLine();
